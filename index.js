be35417a7dc83343f5ed7a974bffb08065921520
Lolisdu <lolidu@yahoo.com.br>
2022-06-30 12:32:43 -0300

    Exercicio 03

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/**
 function funcao2(){
-    document.white('Este texto foi impresso pela função do arquivo externo.');
-  }
+   document.white('Este texto foi impresso pela função do arquivo externo.');
+ }
+*/
+
+
+const btn = document.querySelectorAll("button")
+
+btn[0].addEventListener("click", function teste) {
+  alert("teste 1")
+}
+
+btn[0].addEventListener("click", function () {
+  alert("teste 2")
+})
+
+btn[0].addEventListener("click"( ) => {
+  alert("teste 3")
+})
